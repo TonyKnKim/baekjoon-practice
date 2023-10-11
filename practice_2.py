12f2540eaa125a53b94d971620376aeaba292e4e
kn kim <tony.qn.kim@gmail.com>
2023-10-11 20:55:46 +0900

    second commit

--- practice_2.py
+++ practice_2.py
@@ -4,6 +4,9 @@
 # print(data)
 
 import sys
-data = list(map(int,sys.stdin.readline().split("\n")))
+data = map(int,sys.stdin.readlines())
+data = [data.strip]
 print(max(data))
 print(data.index(max(data))+1)
+
+print("깃허브 추가 수정")
