531debb96c8f67f0da600f9f0ce39ce8db611eaa
kn kim <tony.qn.kim@gmail.com>
2023-10-19 21:43:54 +0900

    231019 algorithm practice commit

--- practice.py
+++ practice.py
@@ -1,3 +1,7 @@
-a=1
+basket_num, try_num, *remains= map(int,open(0).read().split())
+result = list(range(1,6))
 
-print(a)
+while remains:
+    left,right,*remains=remains
+    result[left-1],result[right-1]=result[right-1],result[left-1]
+print(*result)
